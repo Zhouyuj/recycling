be335ff0d7c8a99f60bc7e3b4043f103c1a745d5
zhouyujun <yujun@zhouyujundeMacBook-Pro.local>
2019-05-29 23:54:11 +0800

    (#)手动添加重定向

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const history = require('connect-history-api-fallback');
+// const history = require('connect-history-api-fallback');
 
 // health check url
 app.get('/api/capabilities', function(req, res, next) {
@@ -13,9 +13,29 @@ app.get('/api/capabilities', function(req, res, next) {
  * must be placed before static resource
  * must be placed after logic
  */
-app.use(history());
+// app.use(history());
 // 中间件的资源公开
 app.use(express.static(path.join(__dirname, '../dist')));
+
+// 默认重定向到根目录下
+app.use('/', function(req, res, next) {
+  res.redirect('/');
+});
+
+// app.use(function (req, res, next) {
+//     if (req.path.indexOf('/api') >= 0){
+//         next();
+//     } else { //angular启动页
+//         res.sendfile('web-client/index.html');
+//     }
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 const server = app.listen(9999, function() {
   const host = server.address().address;
